7318fd2b6f97a069998640a90fa8edb8327124c9
Efrain Palacios Mosquera <efpalaciosmo@unal.edu.co>
2024-02-28 01:58:34 +0000

    Update 2 files
    - /swaysh.sh
    - /link.sh

---
+++ link.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm $HOME/.bashrc
+ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc
+ln -s $HOME/.dotfiles/ssh $HOME/.ssh
+ln -s $HOME/.dotfiles/config/bat $HOME/.config/bat
+ln -s $HOME/.dotfiles/config/foot $HOME/.config/foot
+ln -s $HOME/.dotfiles/config/nvim $HOME/.config/nvim
+ln -s $HOME/.dotfiles/config/sway $HOME/.config/sway
+ln -s $HOME/.dotfiles/config/waybar $HOME/.config/waybar
+ln -s $HOME/.dotfiles/config/wallpapers $HOME/.config/wallpapers
+ln -s $HOME/.dotfiles/config/wofi $HOME/.config/wofi
+ln -s $HOME/.dotfiles/config/zathura $HOME/.config/zathura
+ln -s $HOME/.dotfiles/config/starship.toml $HOME/.config/starship.toml
+ln -s $HOME/.dotfiles/config/electron25-flags.conf $HOME/.config/electron25-flags.conf
