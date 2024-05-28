95e8d43751490ad21aed2f972a39f449ab7a23bd
Efrain Palacios Mosquera <efpalaciosmo@unal.edu.co>
2024-05-28 22:01:00 +0000

    Update 3 files
    - /config/kitty/kitty.conf
    - /config/tmux/tmux.conf
    - /link.sh

--- link.sh
+++ link.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Setup github
+git config --global init.defaultBranch main
 git config --global user.name "Efrain Palacios Mosquera"
 git config --global user.email "efpalaciosmo@unal.edu.co"
-git init --initial-branch=main
 
 
 # Setup links
@@ -13,5 +13,6 @@ ln -s $HOME/.dotfiles/ssh $HOME/.ssh
 ln -s $HOME/.dotfiles/config/bat $HOME/.config/bat
 ln -s $HOME/.dotfiles/config/nvim $HOME/.config/nvim
 ln -s $HOME/.dotfiles/config/tmux $HOME/.config/tmux
+ln -s $HOME/.dotfiles/config/kitty $HOME/.config/kitty
 ln -s $HOME/.dotfiles/config/starship.toml $HOME/.config/starship.toml
 ln -s $HOME/.dotfiles/config/electron25-flags.conf $HOME/.config/electron25-flags.conf
